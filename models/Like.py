0adba0a92319d93209cb341ff06e9d1568f650ca
Charalampos Poulikidis <hambos22@gmail.com>
2022-03-16 16:53:54 +0200

    Update Like.py

--- models/Like.py
+++ models/Like.py
@@ -4,13 +4,14 @@ from models.models import Base
 
 like_table = Table('like',
                    Base.metadata,
-                   Column('pk', Integer(), primary_key=True),
+                   #Column('pk', Integer(), primary_key=True),
                    Column('user_id', ForeignKey('user.pk', ondelete='CASCADE'), index=True),
                    Column('post_id', ForeignKey('post.pk', ondelete='CASCADE'), index=True))
 
-
+"""
 class Like(Base):
-    __table__ = like_table
+   __table__ = like_table
 
     def __repr__(self):
         return f"PK:{self.pk}, UserID:{self.user_id}, PostId:{self.post_id}"
+"""
